b2efdd6c43ee1a474418e2f081e8cf07c75e56a1
OAO-MadHatter <larsenj2064@gmail.com>
2022-10-26 17:09:03 -0700

    Module 8 Challenge Commit. #1
    Cleaned up repository added challenge files and code.

--- Queries/crowdfunding_db_schema.sql
+++ Queries/crowdfunding_db_schema.sql
@@ -1,13 +1,13 @@
--- Active: 1666248580896@@localhost@5432@Mod8ETL
+-- Active: 1666248580896@@localhost@5432@crowdfunding_db
 
 CREATE TABLE campaign
 (
     cf_id INT not null,
     contact_id INT not null,
-    company_name VARCHAR not null,
-    description VARCHAR not null,
-    goal INT not null,
-    pledged INT not null,
+    company_name VARCHAR(100) not null,
+    descriptions TEXT not null,
+    goal NUMERIC(10,2) not null,
+    pledged NUMERIC(10,2) not null,
     outcome VARCHAR not null,
     backers_count INT,
     country VARCHAR not null,
@@ -49,15 +49,13 @@ CREATE TABLE subcategory
     UNIQUE (subcategory_id)
 );
 
-ALTER TABLE campaign
+CREATE TABLE backers
 (
-    FOREIGN KEY
-(category_id) REFERENCES category
-(category_id),
-    FOREIGN KEY
-(subcategory_id) REFERENCES subcategory
-(subcategory_id),
-    FOREIGN KEY
-(contact_id) REFERENCES contacts
-(contact_id)
- );
+    backer_id VARCHAR NOT NULL,
+    cf_id INT NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULl,
+    PRIMARY KEY (backer_id),
+    FOREIGN KEY (cf_id) REFERENCES campaign (cf_id)
+);
